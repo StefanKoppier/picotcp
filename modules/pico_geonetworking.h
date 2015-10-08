9933318e014cd5f06604c526a837ecaeed1c349f
unknown <skoppier@PC1015.europe.corp.altran.com>
2015-10-08 16:56:05 +0200

    Added the Basic- and Common Header structs to be used in a later stadium.

--- modules/pico_geonetworking.h
+++ modules/pico_geonetworking.h
@@ -9,31 +9,63 @@
 #define	INCLUDE_PICO_GEONETWORKING
 
 #include "pico_protocol.h"
+#include "pico_config.h"
 
 extern struct pico_protocol pico_proto_geonetworking;
+
+/// The Basic Header is a header present in every GeoNetworking packet.
+PACKED_STRUCT_DEF pico_gn_basic_header 
+{
+    uint8_t version: 4; ///< Identifies the version of the GeoNetworking protocol.
+    uint8_t next_header: 4; ///< Identifies the header immediately following the GeoNetworking Basic Header.
+    uint8_t reserved; ///< Reserved, should be set to 0.
+    uint8_t lifetime; ///< Indicates the maximum tolerable time a packet can be buffered until it reaches its destination.
+    uint8_t remaining_hop_limit; ///< Decrembented by 1 by each GeoAdhoc router that forwards the packet. The packet shall not be forwarded if RHL is decremented to zero.
+};
+
+/// The Common Header is a header present in every GeoNetworking packet.
+PACKED_STRUCT_DEF pico_gn_common_header
+{
+    uint8_t  next_header: 4; ///< Identifies the type of header immediately following the GeoNetworking headers.
+    uint8_t  reserved_1: 4; ///< Reserved should be set to 0.
+    uint8_t  header: 4; ///< Identifies the type of the GeoNetworking extended header.
+    uint8_t  subheader: 4; ///< Identifies the sub-type of the GeoNetworking extended header.
+    uint8_t  traffic_class; ///< Traffic class that represents Facility-layer requirements on packet transport.
+    uint8_t  flags; ///< Bit 0: Indicates whether the ITS-S is mobile or stationary. Bit 1 to 7: Reserved, should be set to 0.
+    uint16_t payload_length; ///< Length of the GeoNetworking payload, i.e. the rest of the packet following the whole GeoNetworking header in octets.
+    uint8_t  maximum_hop_limit; ///< Maximum hop limit, this value is NOT decremented by a GeoAdhoc router that forwards the packet.
+    uint8_t  reserved_2; ///< Reserved, should be set to 0.
+};
+
+/// The combined Basic Header and Common Header which are available in every GeoNetworking packet.
+PACKED_STRUCT_DEF pico_gn_header
+{
+    struct pico_gn_basic_header  basic_header; ///< The Basic Header of the GeoNetworking packet.
+    struct pico_gn_common_header common_header; ///< The Common Header of the GeoNetworking packet.
+};
 
 /// Interface implementation which allows allocation of a GeoNetworking frame.
 ///  \param self The protocol definition, this protocol will always be pico_proto_geonetworking.
 ///  \param size The size of the payload to be allocated.
 ///  \returns An GeoNetworking frame with the header sizes set correctly.
-static struct pico_frame *pico_gn_alloc(struct pico_protocol *self, uint16_t size);
+struct pico_frame *pico_gn_alloc(struct pico_protocol *self, uint16_t size);
 
 /// Interface implementation which allows the processing of incoming frames, from the Data Link Layer.
 ///  \param self The protocol definition, this protocol will always be pico_proto_geonetworking.
 ///  \param f The which needs to be processed.
 ///  \returns 0 on success, -1 on failure.
-static int pico_gn_process_in(struct pico_protocol *self, struct pico_frame *f);
+int pico_gn_process_in(struct pico_protocol *self, struct pico_frame *f);
 
 /// Interface implementation which allows the processing of outgoing frames, from the Transport layer.
 ///  \param self The protocol definition, this protocol will always be pico_proto_geonetworking.
 ///  \param f The which needs to be processed.
 ///  \returns 0 on success, -1 on failure.
-static int pico_gn_process_out(struct pico_protocol *self, struct pico_frame *f);
+int pico_gn_process_out(struct pico_protocol *self, struct pico_frame *f);
 
 /// Interface implementation which allows ???
 ///  \param self The protocol definition, this protocol will always be pico_proto_geonetworking.
 ///  \param f The which needs to be processed.
 ///  \returns 0 on success, -1 on failure.
-static int pico_gn_frame_sock_push(struct pico_protocol *self, struct pico_frame *f);
+int pico_gn_frame_sock_push(struct pico_protocol *self, struct pico_frame *f);
 
 #endif	/* INCLUDE_PICO_GEONETWORKING */
